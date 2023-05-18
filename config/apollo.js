f06687e6b21edf1b71ac8b2d26c4915ba4a9d242
Joel Acef <joelacef@gmail.com>
2023-05-18 18:18:05 -0300

    adding new url

--- config/apollo.js
+++ config/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'https://sleepy-waters-02658.herokuapp.com/',
+    uri: 'https://agile-meadow-64078.herokuapp.com/',
     fetch
 });
 
