92963b80ec47e2316d6c2b8f70d7da2af8f1f772
Joel Acef <joelacef@gmail.com>
2023-05-18 21:29:32 -0300

    push other url

--- config/apollo.js
+++ config/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'https://agile-meadow-64078.herokuapp.com/',
+    uri: 'https://crmclient-narqe.vercel.app/',
     fetch
 });
 
@@ -23,5 +23,7 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: authLink.concat( httpLink )
 })
+
+console.log(client.link);
 
 export default client;
