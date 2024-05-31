9021627c81b34e1bc2669b7ff078baa35be66472
Joel Acef <joelacef@gmail.com>
2024-05-31 12:29:50 -0300

    fix education

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@
 import { Inter } from "next/font/google";
 import "./main.css";
 import '../../i18n';
-import ThemeRegistry from '../utils/ThemeRegistry';
+import { ThemeProvider } from '@mui/material/styles';
+import themeImport from '../utils/theme';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,20 +12,22 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) {  
+  const theme = themeImport;
+
   return (
-    <html lang="en">
-      <head>
-          <title>Joel Acef - CV</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <link rel="stylesheet" type="text/css" media="screen" href="./main.css"/>
-          <link rel="shortcut icon" type="image/png" href="./favicon.png" />
-          <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,400italic,700' rel='stylesheet' type='text/css'/>
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
-      </head>
-      <body className={inter.className}>
-        <ThemeRegistry options={{ key: 'mui-theme' }}>{children}</ThemeRegistry>
-      </body>
-    </html>
+    <ThemeProvider theme={theme}>
+      <html lang="en">
+        <head>
+            <title>Joel Acef - CV</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="stylesheet" type="text/css" media="screen" href="./main.css"/>
+            <link rel="shortcut icon" type="image/png" href="./favicon.png" />
+            <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,400italic,700' rel='stylesheet' type='text/css'/>
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ThemeProvider>
   );
 }
